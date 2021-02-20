983a6a49639c11639952472f1d2ee0fac4ae1d9b
Oksana <solomkao@gmail.com>
2021-02-20 13:17:26 +0200

    Initial commit

---
+++ src/main/java/com/solomka/docker/Homework21DockerApplication.java
@@ -0,0 +1,13 @@
+package com.solomka.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework21DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework21DockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solomka/docker/Homework21DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomka.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework21DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
