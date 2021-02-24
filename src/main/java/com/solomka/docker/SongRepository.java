4caafe9c4064a645009ef5676d7ee983c1453812
Oksana <solomkao@gmail.com>
2021-02-24 19:52:35 +0200

    refactoring

--- src/main/java/com/solomka/docker/SongRepository.java
+++ src/main/java/com/solomka/docker/SongRepository.java
@@ -2,6 +2,5 @@ package com.solomka.docker;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SongRepository extends JpaRepository<Song, Long>
-{
+public interface SongRepository extends JpaRepository<Song, Long>{
 }
